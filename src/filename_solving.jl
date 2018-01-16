77d28fbecd65d236dbd392e5dc5b8c300cfe38cf
Lyndon White <oxinabox@ucc.asn.au>
2018-01-16 19:46:05 +0800

    =fix regex for windows, be harsher for linux, and an output for debugging

--- src/filename_solving.jl
+++ src/filename_solving.jl
@@ -6,23 +6,23 @@ Given a remotepath (URL) returns the filename that it should be saved to locally
 """
 function get_filename(remotepath)
     headers = get_headers(remotepath)
-    filename_match = match(r"Content\-Disposition\:.*filename=(.*)", headers)
-    if filename_match == nothing
+    filename_match = try_get_filename(remotepath)
+    ret = if filename_match == nothing
         # couldn't get it from the headers
         basename(remotepath)
     else
         strip(filename_match[1])
     end
+    @show ret
+    ret
 end
 
 
 downloadcmd = nothing
 
 @static if is_windows()
-    function get_headers(url)
-        # WARNING: On windows this doesn't get all the headers
-        # it only gets content-disposition, which is all we need
-        # If that is not set, then it returns "" (the empty string)
+    "Returns a regex Match or nothing"
+    function try_get_filename(url)
         ps = "C:\\Windows\\System32\\WindowsPowerShell\\v1.0\\powershell.exe"
         prog ="""
             \$request = [System.Net.WebRequest]::Create('$url')
@@ -30,9 +30,22 @@ downloadcmd = nothing
             \$request.Method = "HEAD"
             \$request.GetResponse().Headers["Content-Disposition"]
             """
-        readstring(`$ps -NoProfile -Command "$prog"`)
+        content_disp = readstring(`$ps -NoProfile -Command "$prog"`)
+        # If Content-Disposition is not set, then it returns "" (the empty string)
+        # else returns a single line e.g. "attachment; filename=JuliaCI-Coverage.jl-v0.4.0-13-g35b8b49.tar.gz"
+
+        # can be chill with this regex as the header is fully parsed
+        match(r".*filename=(.*)", content_disp)
     end
 else
+    "Returns a regex Match or nothing"
+    function try_get_filename(url)
+        headers = get_headers(url)
+        # be harsh with the following regex as it is not parsed
+        # so need to make it as hard as possible for a pathological URL to break it.
+        match(r"^\s*Content\-Disposition\:.*filename=(.*)\s*$"m, headers)
+    end
+
     function get_headers(url)
         global downloadcmd
         if downloadcmd === nothing
