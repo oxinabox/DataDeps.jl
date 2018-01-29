e41bd5726f6ee01414ef55103fbd7184fb45383b
Lyndon White <oxinabox@ucc.asn.au>
2018-01-29 13:44:26 +0800

    =extract (slightly) weirders headers

--- src/filename_solving.jl
+++ src/filename_solving.jl
@@ -5,21 +5,21 @@
 Given a remotepath (URL) returns the filename that it should be saved to locally.
 """
 function get_filename(remotepath)
-    filename_match = try
+    filename = try
         try_get_filename(remotepath)
     catch err
         # Catch *everything* here, as we can always recover and there are many things that can go wrong
         warn("Could not resolve filename due to")
         warn(err)
         warn("falling back to using final part of remotepath")
-        filename_match = nothing
+        filename = nothing
     end
 
-    ret = if filename_match == nothing
+    ret = if filename == nothing
         # couldn't get it from the headers
         basename(remotepath)
     else
-        strip(filename_match[1])
+        filename
     end
     ret
 end
@@ -29,10 +29,33 @@ end
     try_get_filename(url)
 
 Uses as HEAD request, to attempt to retrieve the filename from the HTTP headers.
-Returns a regex Match or nothing
+Returns a string or nothing if it failes
 """
 function try_get_filename(url)
     resp = HTTP.request("HEAD", url,  ["User-Agent"=>"DataDeps.jl (http-lib: HTTP.jl; lang: Julia)"]);
     content_disp = get(Dict(resp.headers),"Content-Disposition","");
-    match(r"filename=\"(.*)\"", content_disp);
+    raw = match(r"filename\s*=\s*(.*)", content_disp); # TECHDEBT: Consider if more of this should be moved to process header filename
+
+    process_header_filename(raw)
 end
+
+"""
+    process_header_filename(raw)
+
+Deal with some of the weird and varied ways filenames can be given.
+Not full coverage, but getting the common cases.
+
+Return nothing if input is nothing
+"""
+function process_header_filename(raw::RegexMatch)::String
+    ret = raw[1]
+    quoted_match = match(r"\"(.*)\"", ret)
+    
+    if quoted_match != nothing
+        ret = unescape_string(quoted_match[1]) #It was in quotes, so it will be double escaped
+    end
+    
+    strip(ret)
+end
+
+process_header_filename(::Void) = nothing
--- test/filename_solving.jl
+++ test/filename_solving.jl
@@ -2,10 +2,13 @@ using DataDeps
 using Base.Test
 import DataDeps.get_filename
 
-@testset "not requiring headers" begin
-    @test get_filename("https://www.angio.net/pi/digits/10000.txt") == "10000.txt"
-end
+@testset "post processing" begin
+    phf(x) = DataDeps.process_header_filename(match(r"(.*)",x))
 
+    @test phf("foo.txt") == "foo.txt"
+    @test phf("\"foo.txt\"") == "foo.txt"
+    @test phf("\"bar \\\"zap zing\\\" foo.txt\"") == "bar \"zap zing\" foo.txt"
+end
 
 @testset "using headers" begin
     # From http://test.greenbytes.de/tech/tc2231/
@@ -13,11 +16,19 @@ end
     
     @test "foo.html" == get_filename("http://test.greenbytes.de/tech/tc2231/attwithasciifilename.asis")
 
+    @test "foo.html" == get_filename("http://test.greenbytes.de/tech/tc2231/attwithasciifilenamenq.asis") # Technically invalid, but github API does this
+
     @test "attonly.asis" == get_filename("http://test.greenbytes.de/tech/tc2231/attonly.asis")
 
-
-    @test_broken "\"quoting\" tested.html" == get_filename("http://test.greenbytes.de/tech/tc2231/attwithasciifnescapedquote.asis")
+    @test "\"quoting\" tested.html" == get_filename("http://test.greenbytes.de/tech/tc2231/attwithasciifnescapedquote.asis")
 
     @test_broken "ä-%41.html" == get_filename("http://test.greenbytes.de/tech/tc2231/attwithfilenamepctandiso.asis")
 
 end
+
+
+@testset "not requiring headers" begin
+    @test get_filename("https://www.angio.net/pi/digits/10000.txt") == "10000.txt"
+end
+
+
