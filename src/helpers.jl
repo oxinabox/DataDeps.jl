90d82274d21c67c36d8e25f5729cf831f2af97f2
Lyndon White <lyndon.white@research.uwa.edu.au>
2018-05-07 15:20:52 +0800

    typofix

--- src/helpers.jl
+++ src/helpers.jl
@@ -52,7 +52,7 @@ end
     unpack(f; keep_originals=false)
 
 Extracts the content of an archive in the current directory;
-deleting the original archieve, unless the `keep_originals` flag is set.
+deleting the original archive, unless the `keep_originals` flag is set.
 """
 function unpack(f; keep_originals=false)
     run(unpack_cmd(f, pwd(), last(splitext(f)), last(splitext(first(splitext(f))))))
