3dc374bea6908837ffd60dbf3b17d7d44fe3dbd2
Lyndon White <lyndon.white@research.uwa.edu.au>
2018-05-07 18:29:49 +0800

    Add deprecated tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -14,30 +14,36 @@ for filename in tests
 end
 
 
+examples =  [
+    "examples.jl",
+    "examples_manual.jl",
+    "deprecated_examples.jl",
+    "deprecated_examples_manual.jl",
+]
 
-@testset "examples" begin
-    tempdir = mktempdir()
-    try
-        info("sending all datadeps to $tempdir")
-        withenv("DATADEPS_LOAD_PATH"=>tempdir) do
-            @testset "download and use" begin
-                include("examples.jl")
-                include("examples_manual.jl")
-            end
-            withenv("DATADEPS_DISABLE_DOWNLOAD"=>"true") do
-                @testset "use already downloaded" begin
-                    include("examples.jl")
-                    include("examples_manual.jl")
+@testset "examples" for fn in examples
+    @testset "$fn" begin
+        tempdir = mktempdir()
+        try
+            info("sending all datadeps to $tempdir")
+            withenv("DATADEPS_LOAD_PATH"=>tempdir) do
+                @testset "download and use" begin
+                    include(fn)
+                end
+                withenv("DATADEPS_DISABLE_DOWNLOAD"=>"true") do
+                    @testset "use already downloaded" begin
+                        include(fn)
+                    end
                 end
             end
+        finally
+    		try
+    			info("removing $tempdir")
+    			rm(tempdir, recursive=true, force=true)
+    		catch err
+    			warn("Something went wrong with removing $tempdir")
+    			warn(err)
+    		end
         end
-    finally
-		try
-			info("removing $tempdir")
-			rm(tempdir, recursive=true, force=true)
-		catch err
-			warn("Something went wrong with removing $tempdir")
-			warn(err)
-		end
     end
 end
