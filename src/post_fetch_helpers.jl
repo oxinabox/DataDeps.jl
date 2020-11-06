f9709b66d3c1a6a7c0f7ba03c2772ab63edd8d5f
Aayush Joglekar <iit2017017@iiita.ac.in>
2020-11-06 10:48:46 +0530

    remove unpack_cmd function and add code in unpack

--- src/post_fetch_helpers.jl
+++ src/post_fetch_helpers.jl
@@ -1,16 +1,5 @@
 # This file is a part of DataDeps.jl. License is MIT.
 
-function unpack_cmd(file,directory,extension,secondary_extension)
-    output_dir = first(splitext(file))
-    p7zip() do p7zip_executable_path
-        try
-            run(`$p7zip_executable_path e $file -o$output_dir`)
-        catch err
-            throw(ArgumentError("failed to extract specified file. Please check if the path is correct, if yes, either the file has an unsupported extension or corrupt."))
-        end
-    end
-end
-
 """
     unpack(f; keep_originals=false)
 
@@ -18,7 +7,16 @@ Extracts the content of an archive in the current directory;
 deleting the original archive, unless the `keep_originals` flag is set.
 """
 function unpack(f; keep_originals=false)
-    unpack_cmd(f, pwd(), last(splitext(f)), last(splitext(first(splitext(f)))))
+    file,directory,extension,secondary_extension = f, first(splitext(f)), last(splitext(f)), last(splitext(first(splitext(f))))
+
+    p7zip() do p7zip_executable_path
+        try
+            run(`$p7zip_executable_path e $file -o$directory`)
+        catch err
+            throw(ArgumentError("failed to extract specified file. Please check if the path is correct, if yes, either the file has an unsupported extension or corrupt."))
+        end
+    end
+
     rm("pax_global_header"; force=true)# Non-compliant tarball extractors dump out this file. It is meaningly (google it's filename for more)
     !keep_originals && rm(f)
 end
