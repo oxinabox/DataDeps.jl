f37221615d540a2399decbf81c633dec9de213b3
Aayush Joglekar <iit2017017@iiita.ac.in>
2020-11-11 19:28:12 +0530

    Add comments and split lines

--- src/post_fetch_helpers.jl
+++ src/post_fetch_helpers.jl
@@ -7,16 +7,26 @@ Extracts the content of an archive in the current directory;
 deleting the original archive, unless the `keep_originals` flag is set.
 """
 function unpack(f; keep_originals=false)
-    file,directory,extension,secondary_extension = f, first(splitext(f)), last(splitext(f)), last(splitext(first(splitext(f))))
-
+    file = f # /home/hello/xyz.zip  |   /home/hello/xyz.tar.gz
+    directory = first(splitext(f)) # /home/hello/xyz  |   /home/hello/xyz.tar
+    extension = last(splitext(f)) # .zip    |   .gz
+    secondary_extension = last(splitext(first(splitext(f)))) #  ""  |   .tar
+    # p7zip() returns the path of 7z exectuable
     p7zip() do p7zip_executable_path
         try
             if secondary_extension == ""
+                # for files with a single compression like .zip, .rar, etc.
                 run(`$p7zip_executable_path e $file -o$directory -y`)
             else
+                # files with secondary extension like .tar.gz, 
+                # first extract .tar.gz to .tar in the directory where .tar.gz was present
+                # this .tar is the intermediate file 
+                # splitext("/home/filename.tar.gz") returns "/home/filename.tar" which is filename of our intermediate file
                 intermediate_dir, intermediate_file = first(splitdir(file)), directory
+                # directory in which data is to be extracted is split again
+                # "/home/filename.tar" is converted to "/home/filename"
                 directory = first(splitext(intermediate_file))
-
+                # check out 7z docs for more information
                 # 7z x creates intermediate compressed file
                 run(`$p7zip_executable_path x $file -o$intermediate_dir -y`)
                 # 7z e extracts the intermediate file and places content in a directory
@@ -25,10 +35,9 @@ function unpack(f; keep_originals=false)
                 rm(intermediate_file)
             end
         catch err
-            throw(ArgumentError("failed to extract specified file. Please check if the path is correct, if yes, either the file has an unsupported extension or corrupt."))
+            @warn "failed to extract specified file. Please check if the path is correct, if yes, either the file has an unsupported extension or corrupt."
+            rethrow(err)
         end
     end
-
-    rm("pax_global_header"; force=true)# Non-compliant tarball extractors dump out this file. It is meaningly (google it's filename for more)
     !keep_originals && rm(f)
 end
