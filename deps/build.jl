d976143fe3af67f9bda5fbbac0fd592cd0520e70
Carlo Lucibello <carlo.lucibello@gmail.com>
2022-07-28 08:42:25 +0200

    drop julia < 1.6 and BinaryBuilder

--- deps/build.jl
+++
@@ -1,3 +0,0 @@
-if VERSION < v"1.3"
-    include("build_p7zip.v16.2.0.jl")
-end
--- src/DataDeps.jl
+++ src/DataDeps.jl
@@ -1,19 +1,6 @@
 # This file is a part of DataDeps.jl. License is MIT.
 module DataDeps
-
-if VERSION < v"1.3"
-    # Load in `deps.jl`, complaining if it does not exist
-    const depsjl_path = joinpath(@__DIR__, "..", "deps", "deps.jl")
-    if !isfile(depsjl_path)
-        error("DataDeps not installed properly, run `] build DataDeps`, restart Julia and try again")
-    end
-    include(depsjl_path)
-
-    # Emulate `p7zip_jll` behaviour
-    p7zip(f) = f(p7zip_exe)
-else
-    using p7zip_jll
-end
+using p7zip_jll
 
 using HTTP
 using Reexport
