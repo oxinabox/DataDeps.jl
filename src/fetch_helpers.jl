696fad8137fd4ed0973a4f7896e2294473012336
hhaensel <helmut.haensel@gmx.com>
2026-06-09 13:01:22 +0200

    migrate from HTTP.downloads() to Downloads.downloads()

--- src/fetch_helpers.jl
+++ src/fetch_helpers.jl
@@ -41,21 +41,21 @@ end
 """
   fetch_base(remote_path, local_dir)
 
-Download from `remote_path` to `local_dir`, via `Base` mechanisms.
-The download is performed using `Base.download`
+Download from `remote_path` to `local_dir`, via stdlib Downloads.
+The download is performed using `Downloads.download`
 and `Base.basename(remote_path)` is used to determine the filename.
 This is very limited in the case of HTTP as the filename is not always encoded in the URL.
 But it does work for simple paths like `"http://myserver/files/data.csv"`.
 In general for those cases prefer `http_download`.
 
 The more important feature is that this works for anything that has overloaded
-`Base.basename` and `Base.download`, e.g. [`AWSS3.S3Path`](https://github.com/JuliaCloud/AWSS3.jl).
+`Base.basename` and `Downloads.download`, e.g. [`AWSS3.S3Path`](https://github.com/JuliaCloud/AWSS3.jl).
 While this doesn't work for all transport mechanisms (so some datadeps will still a custom `fetch_method`),
 it works for many.
 """
 function fetch_base(remote_path, local_dir)
     localpath = joinpath(local_dir, basename(remote_path))
-    return Base.download(remote_path, localpath)
+    Downloads.download(remote_path, localpath)
     return string(localpath)
 end
 
@@ -77,5 +77,58 @@ By default it is once per second, though this depends on configuration
 """
 function fetch_http(remotepath, localdir; update_period=progress_update_period())
     @assert(isdir(localdir))
-    return HTTP.download(remotepath, localdir; update_period=update_period, decompress=false)
+
+    local downloaded_bytes = 0
+    local total_bytes = 0
+    local last_update_time = time()
+    local filename = Downloads.url_filename(remotepath)
+
+    # Progress callback with throttling based on update_period
+    progress_callback = function(total, now)
+        total_bytes = total
+        downloaded_bytes = now
+
+        current_time = time()
+        if !isinf(update_period) && (current_time - last_update_time) >= update_period
+            if filename === nothing
+                # Don't have filename yet
+                if total > 0
+                    progress_pct = round(100 * now / total; digits=1)
+                    @info "Downloading: $(now) / $(total) bytes ($(progress_pct)%)"
+                else
+                    @info "Downloading: $(now) bytes"
+                end
+            else
+                if total > 0
+                    progress_pct = round(100 * now / total; digits=1)
+                    @info "Downloading $filename: $(now) / $(total) bytes ($(progress_pct)%)"
+                else
+                    @info "Downloading $filename: $(now) bytes"
+                end
+            end
+            last_update_time = current_time
+        end
+    end
+
+    # Download to temporary location first
+    # Downloads.download without output path uses Content-Disposition or URL basename
+    tempfile = Downloads.download(remotepath; progress = progress_callback)
+
+    # Get the filename that was determined by Downloads
+    filename = basename(tempfile)
+
+    # Move to the target directory
+    localpath = joinpath(localdir, filename)
+    mv(tempfile, localpath; force=true)
+
+    # Final progress log
+    if !isinf(update_period)
+        if total_bytes > 0
+            @info "Downloaded $filename: $(downloaded_bytes) / $(total_bytes) bytes (complete)"
+        else
+            @info "Downloaded $filename: $(downloaded_bytes) bytes (complete)"
+        end
+    end
+
+    return string(localpath)
 end
